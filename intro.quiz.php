1d231b3c3a6980d9ff08a607000a248b27363659
aishava <aisha.ambare@edu-kw1c.nl>
2023-11-07 14:39:08 +0100

    Introductie pagina zonder opmaak

---
+++ intro.quiz.php
@@ -0,0 +1,24 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Introductie Calaminator</title>
+</head>
+<body>
+
+<h1 style="text-align: center; margin-bottom: 85px; padding-top: 50px; font-size: 100px;"> Introductie Calaminator </h1>
+
+<hr style="    height: 10px; background-color: #333;">
+
+<p style="text-align: center; padding-top: 55px;" >
+    Hey! Fijn dat je de tijd neemt om deze test te maken. In deze test krijg je informatie over hoe je met <br>
+    calamiteiten moet omgaan en kun je ook voor jezelf zien hoe jouw kennis ervoor staat. Alvast succes!😊
+</p>
+
+<a href="Calaminator.php" style="font-size: 50px; padding: 20px; position: relative; left: 45rem; bottom: -20rem; display: inline-block; background-color: white; border: 1px solid black; border-radius: 10px; cursor: pointer; text-align: center; color: black; text-decoration: none;">Start</a>
+
+
+</body>
+</html>
