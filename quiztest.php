4481bc082e1ef1f3d1f586e138b52cef53e148bb
kappugino <tygo.bovens@edu-kw1c.nl>
2023-11-08 13:19:14 +0100

    quiztest

--- quiztest.php
+++ quiztest.php
@@ -1,31 +1,54 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Toggle Element with JavaScript</title>
+    <title>quiztest</title>
     <style>
-        #myElement {
+        .hidden {
             display: none;
         }
     </style>
 </head>
 <body>
-<button id="toggleButton">volgende</button>
+<div id="startQuiz">
+    <h1>
+        uitleg quiz
+    </h1>
+    <button id="start">begin</button>
+</div>
 
-<div id="myElement">
-    This is a hidden element.
+<div id="vraag_1" class="hidden">
+    vraag 1<br>
+    <button id="volgende">volgende</button>
+</div>
+
+<div id="antwoord_1" class="hidden">
+    antwoord 1<br>
+    <button id="volgende2">volgende</button>
 </div>
 
 <script>
-    const toggleButton = document.getElementById('toggleButton');
-    const myElement = document.getElementById('myElement');
+    const startButton = document.getElementById('start');
+    const startQuiz = document.getElementById('startQuiz');
+    const vraag_1 = document.getElementById('vraag_1');
+    const volgendeButton = document.getElementById('volgende');
+    const antwoord_1 = document.getElementById('antwoord_1');
+    const volgendeButton2 = document.getElementById('volgende2');
 
-    toggleButton.addEventListener('click', function () {
-        if (myElement.style.display === 'none') {
-            myElement.style.display = 'block';
-        } else {
-            myElement.style.display = 'none';
-        }
+    startButton.addEventListener('click', function () {
+        startQuiz.classList.add('hidden'); // Hide the introduction
+        vraag_1.classList.remove('hidden'); // Show question 1
     });
+
+    volgendeButton.addEventListener('click', function () {
+        vraag_1.classList.add('hidden'); // Hide question 1
+        antwoord_1.classList.remove('hidden'); // Show answer 1
+    });
+
+    volgendeButton2.addEventListener('click', function () {
+        antwoord_1.classList.add('hidden'); // Hide answer 1
+        // Add code to show the next element or finish the quiz
+    });
+
 </script>
 </body>
 </html>
