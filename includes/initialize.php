12cc6dc2e457968cd7f7161ff3ddddad8d23261f
EvanGits <evan.geerts@caiway.net>
2023-11-15 14:27:20 +0100

    http host

--- includes/initialize.php
+++ includes/initialize.php
@@ -3,12 +3,14 @@
     $include_end = strpos($_SERVER['SCRIPT_NAME'], '/includes') + 16;  
 
     /*project folder */ 
-    $doc_root = $_SERVER['SERVER_NAME']; 
+    $doc_root = $_SERVER['HTTP_HOST']; 
     
     /*haalt dynamisch rootlocatie + project folder op met $doc_root. */  
     define("WWW_ROOT", $doc_root); 
 
     require_once('functions.php'); 
+
+    echo $doc_root; 
 
     
 ?>
